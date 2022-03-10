8483db22ff4b25b1d62971ed94a9e793e7292c1d
usman asif <57517713+Usmanasif1997@users.noreply.github.com>
2022-03-10 17:41:46 +0500

    fixing the contact api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,30 +1,34 @@
-export default function (req, res) {
-    require('dotenv').config()
-    
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "smtp.gmail.com",
-      auth: {
-        user: 'usmanasifdev@gmail.com',
-        pass: process.env.password,
-      },
-      secure: true,
-    })
-    const mailData = {
-      from: 'usmanasifdev@gmail.com',
-      to: 'jusmanasif435@gmail.com',
-      subject: `Message From Usman Asif Dev`,
+export default async function (req, res) {
+  require("dotenv").config();
+
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "usmanasifdev@gmail.com",
+      pass: process.env.password,
+    },
+    secure: true,
+  });
+  const mailData = {
+    from: "usmanasifdev@gmail.com",
+    to: "jusmanasif435@gmail.com",
+    subject: `Message From Usman Asif Dev`,
     //   text: req.body.message + " | Sent from: " + req.body.email,
-      html: `<div>${req.body.message}</div><p>Sent from:
-      ${req.body.email}</p>`
-    }
+    html: `<div>${req.body.message}</div><p>Sent from:
+      ${req.body.email}</p>`,
+  };
+  await new Promise((resolve, reject) => {
     transporter.sendMail(mailData, function (err, info) {
-      if(err)
-        console.log(err)
-      else
-        console.log(info)
-    })
-    res.status(200)
-  }
-  
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+  res.status(200).json({ status: "OK" });
+}
